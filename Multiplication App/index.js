a96a2f612100542d5d97000eedf3ccbdf425d251
Sakshi Agarwal <sakshiagarwal0902@gmail.com>
2023-02-01 22:53:20 +0530

    Multiplication App javascript changes

---
+++ Multiplication App/index.js
@@ -0,0 +1,32 @@
+const questionE1 =  document.getElementById("question")
+const scoreE1 = document.getElementById("score")
+const formE1 = document.getElementById("form")
+const inputE1 = document.getElementById("input")
+
+const num1 = Math.ceil(Math.random()*10);
+const num2 = Math.ceil(Math.random()*10);
+questionE1.innerHTML = `what is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+let score = JSON.parse(localStorage.getItem("score"));
+
+if(!score){
+    score = 0;
+}
+
+scoreE1.innerHTML = `score: ${score}`
+
+formE1.addEventListener("submit", ()=>{
+    const userAns = +inputE1.value;
+    if(userAns === correctAns){
+        score++;
+        updateLocalStorage();
+    }else{
+        score--;
+        updateLocalStorage();
+    }
+});
+
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score))
+}
